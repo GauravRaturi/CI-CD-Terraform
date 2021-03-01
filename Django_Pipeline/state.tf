8ee0839f0ca298312283563709b3b66d0554f675
Gaurav Raturi <52732421+GauravRaturi@users.noreply.github.com>
2021-03-01 18:09:43 +0530

    Update state.tf

--- Django_Pipeline/state.tf
+++ Django_Pipeline/state.tf
@@ -4,8 +4,6 @@ terraform{
         encrypt = true
         key = "terraform.tfstate"
         region = "ap-south-1"
-        access_key = "AKIAIPU2EDAKKN5FTNVQ"
-        secret_key = "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"
         
     }
 }
